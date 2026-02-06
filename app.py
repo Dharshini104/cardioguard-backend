af23497af9b711755b1f8e712c54bbb9afb9bd55
Dharshini <dharshini.2401038@srec.ac.in>
2026-02-06 18:30:47 +0530

    Configure app for Render deployment

--- app.py
+++ app.py
@@ -102,6 +102,9 @@ def history():
     data = list(predictions.find({}, {"_id": 0}).sort("timestamp", -1))
     return jsonify(data)
 
-# ---------------- RUN ----------------
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
